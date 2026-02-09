522adbfd20b02c0cf8d51b39bef0298b7db6d8c1
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-02-09 21:41:53 +0800

    added required pages

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def recipes_list(request):
+    return render(request, 'recipes_list.html')
+
+def recipe_1(request):
+    return render(request, 'recipes_1.html')
+
+def recipe_2(request):
+    return render(request, 'recipes_2.html')
