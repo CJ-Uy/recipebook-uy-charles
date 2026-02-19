5c772a8abcd6131bdcf1d845e1ab957d51512077
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-02-19 19:15:22 +0800

    removed index route

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import index, recipe, recipes_list
+from .views import recipe, recipes_list
 
 urlpatterns = [
-    path("", index, name="index"),
     path("recipes/ist", recipes_list, name="recipes-list"),
     path("recipe/<int:recipe_id>", recipe, name="recipe"),
 ]
