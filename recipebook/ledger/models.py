5635c1c0ec9d700388b84b2c7870296d7484a449
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-02-19 17:44:34 +0800

    made migration and migrated models to db

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,1 +1,21 @@
-# Create your models here.
+from django.db import models
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
