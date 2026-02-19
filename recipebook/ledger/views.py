2ae4226ec8816a7e5dadcac9975a4fd6c289fbc9
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-02-19 18:55:34 +0800

    modified /recipes/list to use model query

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,60 +1,6 @@
 from django.shortcuts import render
 
-recipes_list_context = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {"name": "tomato", "quantity": "3pcs"},
-                {"name": "onion", "quantity": "1pc"},
-                {"name": "pork", "quantity": "1kg"},
-                {"name": "water", "quantity": "1L"},
-                {"name": "sinigang mix", "quantity": "1 packet"},
-            ],
-            "link": "/recipe/1",
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {"name": "garlic", "quantity": "1 head"},
-                {"name": "onion", "quantity": "1pc"},
-                {"name": "vinegar", "quantity": "1/2cup"},
-                {"name": "water", "quantity": "1 cup"},
-                {"name": "salt", "quantity": "1 tablespoon"},
-                {"name": "whole black peppers", "quantity": "1 tablespoon"},
-                {"name": "pork", "quantity": "1 kilo"},
-            ],
-            "link": "/recipe/2",
-        },
-    ]
-}
-
-recipe_1_context = {
-    "name": "Recipe 1",
-    "ingredients": [
-        {"name": "tomato", "quantity": "3pcs"},
-        {"name": "onion", "quantity": "1pc"},
-        {"name": "pork", "quantity": "1kg"},
-        {"name": "water", "quantity": "1L"},
-        {"name": "sinigang mix", "quantity": "1 packet"},
-    ],
-    "link": "/recipe/1",
-}
-
-
-recipe_2_context = {
-    "name": "Recipe 2",
-    "ingredients": [
-        {"name": "garlic", "quantity": "1 head"},
-        {"name": "onion", "quantity": "1pc"},
-        {"name": "vinegar", "quantity": "1/2cup"},
-        {"name": "water", "quantity": "1 cup"},
-        {"name": "salt", "quantity": "1 tablespoon"},
-        {"name": "whole black peppers", "quantity": "1 tablespoon"},
-        {"name": "pork", "quantity": "1 kilo"},
-    ],
-    "link": "/recipe/2",
-}
+from ledger.models import Recipe
 
 
 def index(request):
@@ -62,7 +8,7 @@ def index(request):
 
 
 def recipes_list(request):
-    return render(request, "recipes_list.html", recipes_list_context)
+    return render(request, "recipes_list.html", {"recipes": Recipe.objects.all()})
 
 
 def recipe_1(request):
