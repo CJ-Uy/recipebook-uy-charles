c35be193ff3e0fa961476116769ffc02baa900c1
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-03-02 21:43:18 +0800

    added profile model and relations

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.CharField(max_length=255)
 
 
 class Ingredient(models.Model):
@@ -11,6 +18,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
