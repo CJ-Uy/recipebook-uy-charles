fe2e647cac01995b6f2894f83b728903f7c86f16
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-03-12 15:08:48 +0800

    added images to recipe admin panel

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,15 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Recipe, RecipeIngredient, Profile
+from .models import Recipe, RecipeIngredient, RecipeImage, Profile 
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
 
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 
 class ProfileInline(admin.StackedInline):
