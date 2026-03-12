4eba1ad4df581c0674f4078aa0dca543789beb5d
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-03-12 16:34:26 +0800

    added saving functionality to /recipe/add

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from ledger.models import Recipe
+from ledger.models import Recipe, Ingredient, RecipeIngredient, Profile
 
 
 def index(request):
@@ -18,4 +18,19 @@ def recipe(request, recipe_id):
 
 @login_required
 def add_recipe(request):
-    return render(request, "addRecipe.html")
+    allIngredients = Ingredient.objects.all()
+    if request.method == "POST":
+        newRecipe = Recipe()
+        newRecipe.name = request.POST.get("name")
+        newRecipe.author = Profile.objects.get(pk=str(request.user.id)) 
+        newRecipe.save()
+        
+        for key, value in request.POST.items():
+            if key.startswith("ingredient:") and value != "":
+                newRecipeIngredient = RecipeIngredient()
+                newRecipeIngredient.ingredient = Ingredient.objects.get(pk=str(key[11:]))
+                newRecipeIngredient.recipe = Recipe.objects.get(pk=newRecipe.pk)
+                newRecipeIngredient.quantity = value
+                newRecipeIngredient.save()
+    
+    return render(request, "addRecipe.html", {"ingredients": allIngredients})
